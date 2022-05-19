7228f7d851a4baf3605c9f82bc1e501921e14de7
Miguel Marin <miguelmarinjavier5@gmail.com>
2022-05-19 17:05:50 -0600

    hamburger

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -17,3 +17,11 @@ datefield.innerHTML = `<em>${fulldate}</em>`;
 document.querySelector(
 	"#lastmodified"
 ).textContent = `${document.lastModified}`;
+
+function toggleMenu() {
+	document.getElementById('primaryNav').classList.toggle('open');
+	document.getElementById('hamburgerBtn').classList.toggle('open');
+}
+
+const x = document.getElementById('hamburgerBtn')
+x.onclick = toggleMenu;
