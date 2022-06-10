75423aa60cc0d7bb5379604b984c4e7cd547ff29
Miguel Marin <miguelmarinjavier5@gmail.com>
2022-06-10 00:50:31 -0600

    css

--- lesson8/js/script.js
+++ lesson8/js/script.js
@@ -1,34 +1,10 @@
 let date = new Date();
-let year = date.getFullYear();
-document.getElementById('year').innerHTML = year;
 
-const images = document.querySelectorAll("[data-src]");
+const datefield = document.querySelector("#date");
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	date
+);
 
-function preloadImage(img) {
-    const src = img.getAttribute("data-src");
-    if(!src) {
-        return;
-    }
+datefield.innerHTML = `<em>${fulldate}</em>`;
 
-    img.src = src;
-}
-
-const imgOptions = {
-    threshold: 0,
-    rootMargin: "0px 0px -100px 0px"
-};
-
-const imgObserver = new IntersectionObserver((entries, ingObserver) => {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            preloadImage(entry.target);
-            imgObserver.unobserve(entry.target);
-        }
-    })
-}, imgOptions);
-
-images.forEach(image => {
-    imgObserver.observe(image);
-});
+document.querySelector("#date").textContent = `${document.lastModified}`;
