4af4a9f034f155c709180ba56b8cd16a2a15c0e1
Miguel Marin <miguelmarinjavier5@gmail.com>
2022-06-23 23:35:19 -0600

    weather

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,28 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = `http://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=Imperial&appid=089a0425418df44f4f7f73e6e3f15ad9`;
+  
+apiFetch(url);
+
+async function apiFetch(apiURL) {
+    const response = await fetch(apiURL);
+    if (response.ok) {
+      const data = await response.json();
+      displayResults(data);
+    } else {
+        throw Error(await response.text());
+    }
+};
+
+function displayResults(weatherData) {
+    currentTemp.innerHTML = weatherData.main.temp.toFixed(1);
+
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+}
